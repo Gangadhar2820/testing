380ed177782208db74bc3779563e6539822b07e7
Rongala Gangadhar <rgan9562@gmail.com>
2024-06-14 09:59:44 +0530

    second

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const cors = require("cors");
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
+const { MongoClient } = require("mongodb");
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  console.log("success")
-  res.end("Hello, World!\n");
+const URL =
+  "mongodb+srv://gangadhar2820:Ganga2820@apepdcl.zxhcewu.mongodb.net/" +
+  "?retryWrites=true&w=majority&appName=apepdcl";
 
+const client = new MongoClient(URL);
+
+app.get("/", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    const dbName = "apepdclConsumers";
+    const collectionName = "B232_CONSUMERS";
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    const result = await collection.findOne({ SERVICE_NO: "000012" });
+    console.log(result);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
 });
 
-server.listen(PORT,()=>{console.log("server started successfully")});
+app.listen(PORT, () => {
+  console.log(`server started successfully on port ${PORT}`);
+});
